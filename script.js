b8edffcded653f2d1c8b5e0986da8d51fb64c27f
Sophie <sophie88leigh@gmail.com>
2021-02-12 16:18:23 -0500

    edit text, added else if to function

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function click() { 
+function submit() { 
     var text = parseInt(document.getElementById("inp").value);
     var output = document.getElementById("output");
    
-    if (text >= 21) {
-        output.innerHTML = "You are 21 or older, please drink responsibly";
-    } else{
-        output.innerHTML = "You are not of legal age";
+    if (text > 21) {
+        output.innerHTML = "You are over 21. Please drink responsibly!";
+    } else if (text == 21) {
+        output.innerHTML = "You are 21. Please drink responsibly!"
     }
-};
+    else{
+        output.innerHTML = "Sorry, You are NOT of legal drinking age";
+    }
+}
